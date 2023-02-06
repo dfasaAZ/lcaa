c3387f67baf2a22e41ac91ab47c50dc42e45e639
DovgalCorparation <just.huk@yandex.ru>
2023-02-06 21:59:38 +0300

    Ended the main (PC)

--- js/index.js
+++ js/index.js
@@ -1,11 +1,3 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    document.querySelector(".header_button").addEventListener("mouseover",()=>{
-        document.querySelector(".header_button").classList.add("active");
-        document.querySelector(".header_button div").classList.add("active");
-    })
-
-    document.querySelector(".header_button").addEventListener("mouseout",()=>{
-        document.querySelector(".header_button").classList.remove("active");
-        document.querySelector(".header_button div").classList.remove("active");
-    })
+    
 })
