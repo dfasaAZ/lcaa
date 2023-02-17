bae171e744c6a43e3a6ea3459eb299e99b8819ad
DovgalCorparation <just.huk@yandex.ru>
2023-02-17 22:02:13 +0300

    Fixed main section of indexPage

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    
+    setTimeout(()=>{
+        document.querySelector(".main_text.p1").style.animation = "none";
+        document.querySelector(".main_text.p1").style.border = "none";
+        document.querySelector(".main_text.p2").style.display = "flex";
+    },6000)
 })
