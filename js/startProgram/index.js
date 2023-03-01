e7136b7d393c0125abf75f318309fea369b2e043
DovgalCorparation <just.huk@yandex.ru>
2023-03-01 23:41:17 +0300

    Correct the massive diaposon in slider

--- js/startProgram/index.js
+++ js/startProgram/index.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+    document.querySelector("#uploadButton audio").volume = 0.1;
+    
     let uploadButton = document.querySelector("#uploadButton");
 
     uploadButton.addEventListener("mouseover", ()=>{
@@ -8,17 +10,20 @@ document.addEventListener("DOMContentLoaded", ()=>{
     let sliderLeftButton = document.querySelector(".sliderButton-left");
     let sliderRightButton = document.querySelector(".sliderButton-right");
     let currentFrame = document.querySelector(".slides");
+    let slidesWindows = document.querySelectorAll(".slides>div");
     let sliderIndex = 0;
+
     sliderRightButton.addEventListener('click', ()=>{
-        if (sliderIndex < 2) sliderIndex++;
+        if (sliderIndex < slidesWindows.length - 1) sliderIndex++;
         currentFrame.style.right = `calc(100% * ${sliderIndex})`;
-        sliderIndex==2 ? sliderRightButton.classList.add("active") : sliderRightButton.classList.remove("active");
+        sliderIndex== slidesWindows.length - 1 ? sliderRightButton.classList.add("active") : sliderRightButton.classList.remove("active");
         sliderIndex!=0 ? sliderLeftButton.classList.remove("active") : sliderLeftButton.classList.add("active");
     })
+
     sliderLeftButton.addEventListener('click', ()=>{
         if (sliderIndex > 0) sliderIndex--;
         currentFrame.style.right = `calc(100% * ${sliderIndex})`;
-        sliderIndex==2 ? sliderRightButton.classList.add("active") : sliderRightButton.classList.remove("active");
+        sliderIndex==slidesWindows.length - 1 ? sliderRightButton.classList.add("active") : sliderRightButton.classList.remove("active");
         sliderIndex==0 ? sliderLeftButton.classList.add("active") : sliderLeftButton.classList.remove("active");
     })
 
