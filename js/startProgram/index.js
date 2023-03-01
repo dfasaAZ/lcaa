373393a4f85e481c0870a829a81bf885e4c3e564
DovgalCorparation <just.huk@yandex.ru>
2023-03-01 23:31:24 +0300

    Make a slider

--- js/startProgram/index.js
+++ js/startProgram/index.js
@@ -7,9 +7,19 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     let sliderLeftButton = document.querySelector(".sliderButton-left");
     let sliderRightButton = document.querySelector(".sliderButton-right");
-    let currentFrame = document.querySelector(".slides>div");
+    let currentFrame = document.querySelector(".slides");
+    let sliderIndex = 0;
     sliderRightButton.addEventListener('click', ()=>{
-        currentFrame.style.marginLeft += "calc(100% * 2 / 3)";
+        if (sliderIndex < 2) sliderIndex++;
+        currentFrame.style.right = `calc(100% * ${sliderIndex})`;
+        sliderIndex==2 ? sliderRightButton.classList.add("active") : sliderRightButton.classList.remove("active");
+        sliderIndex!=0 ? sliderLeftButton.classList.remove("active") : sliderLeftButton.classList.add("active");
+    })
+    sliderLeftButton.addEventListener('click', ()=>{
+        if (sliderIndex > 0) sliderIndex--;
+        currentFrame.style.right = `calc(100% * ${sliderIndex})`;
+        sliderIndex==2 ? sliderRightButton.classList.add("active") : sliderRightButton.classList.remove("active");
+        sliderIndex==0 ? sliderLeftButton.classList.add("active") : sliderLeftButton.classList.remove("active");
     })
 
 
