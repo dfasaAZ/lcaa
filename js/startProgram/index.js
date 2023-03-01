4d88ac61c50db86a9f5f1924cc6bb67dcdf050dd
DovgalCorparation <just.huk@yandex.ru>
2023-03-01 20:57:00 +0300

    Last Saves 20:56

--- js/startProgram/index.js
+++ js/startProgram/index.js
@@ -4,4 +4,13 @@ document.addEventListener("DOMContentLoaded", ()=>{
     uploadButton.addEventListener("mouseover", ()=>{
         document.querySelector("#uploadButton audio").play();
     })
+
+    let sliderLeftButton = document.querySelector(".sliderButton-left");
+    let sliderRightButton = document.querySelector(".sliderButton-right");
+    let currentFrame = document.querySelector(".slides>div");
+    sliderRightButton.addEventListener('click', ()=>{
+        currentFrame.style.marginLeft += "calc(100% * 2 / 3)";
+    })
+
+
 })
