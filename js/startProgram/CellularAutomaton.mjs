99270e56bc408c4ec59db3f0fd349e2001ed863a
dfasaAZ <dfasaAZ@yandex.ru>
2023-03-09 22:11:56 +0300

    got rid of \r then uploading file

--- js/startProgram/CellularAutomaton.mjs
+++ js/startProgram/CellularAutomaton.mjs
@@ -33,7 +33,7 @@ export class CellularAutomaton {
   */
   extractData(csvString) {
     const results = [];
-
+    csvString.replace(/\r/g, "");
     const rows = csvString.split("\n"); // Split string into rows
     const headers = rows.shift().split(","); // Remove first row and use it as headers
     
