e69573f4b1d07540153fa3428f854c0e4d65dd5d
DovgalNikita <nikitadovgalkubsau@yandex.ru>
2023-03-12 17:18:49 +0300

    Изменения под кусок кода Матвея

--- js/startProgram/backend.js
+++ js/startProgram/backend.js
@@ -1,11 +1,9 @@
 import {CellularAutomaton} from './CellularAutomaton.mjs'
 
-
 document.addEventListener("DOMContentLoaded", ()=>{
-//переменные--------------------------------------------
+/**переменные--------------------------------------------*/
     let fR = new FileReader();
     let nameObjectWindow = document.querySelector('.nameObjectWindow');
-    let ctx = document.getElementById('chartModel');
     let obj = new CellularAutomaton();
     let file = document.querySelector("#uploadButton>input");
     let viewDataWindow = document.querySelector(".viewDataWindow");
@@ -16,21 +14,56 @@ document.addEventListener("DOMContentLoaded", ()=>{
     let programCalculateWindow = document.querySelector(".programCalculate");
     let sliderButtons = document.querySelector(`div[class^="sliderButton"]`);
     let programCalculateTitle = document.querySelector(`.titleWindow.main`);
-//событие по нажатии кнопки upload--------------------------------------------
+    let ctx = document.getElementById('chartModel');
+    let greenColorA = "rgb(0, 100, 0, 0.5)";
+    let greenColor = "rgb(0, 100, 0)";
+    let yellowColorA = "rgb(255, 215, 0, 0.5)";
+    let yellowColor = "rgb(255, 215, 0)";
+    let redColorA = "rgb(150, 0, 0, 0.5)";
+    let redColor = "rgb(150, 0, 0)";
+    let numbersArrForChart = []; //для графика значения
+    let labelsArrForChart = [];  //для графика легенды
+    let backgroundColorsArrForChart = [];  //для background-color столбцов
+    let borderColorsArrForChart = [];  //для border-color столбцов
+    let minNumbersArr;
+    let maxNumbersArr;
+/**событие по нажатии кнопки upload--------------------------------------------*/
     file.addEventListener("change", function(){
         try{
+            /** Здесь происходят все основные вычисления массивов */
             function getValues(str){
                 let i=0;
                 str.forEach(e => {
                     i++;
-                    viewDataWindow.innerHTML += `${i}) ${String(e.date).substr(4,11)}::${e.value}<br>`;
-                    console.log(Math.round(e.value));
+                    viewDataWindow.innerHTML += `${i}) ${String(e.date).substr(4,11)}::${e.value}<br>`; // запись в окно 2 шага
+                    numbersArrForChart.push(Math.round(e.value)); 
+                    labelsArrForChart.push(String(e.date).substr(4,11));
                 });
+
+                maxNumbersArr = Math.max.apply(null, numbersArrForChart);
+                minNumbersArr = Math.min.apply(null, numbersArrForChart);
+
+                numbersArrForChart.forEach (e=>{
+                    if (e < minNumbersArr + (maxNumbersArr-minNumbersArr)/3){
+                        backgroundColorsArrForChart.push(redColorA);
+                        borderColorsArrForChart.push(redColor);
+                    }
+                    else if ( e >= minNumbersArr + (maxNumbersArr-minNumbersArr) / 3 && e < minNumbersArr + (maxNumbersArr-minNumbersArr) * 2 / 3) {
+                        backgroundColorsArrForChart.push(yellowColorA);
+                        borderColorsArrForChart.push(yellowColor);
+                    }
+                    else {
+                        backgroundColorsArrForChart.push(greenColorA);
+                        borderColorsArrForChart.push(greenColor);
+                    }
+                })
             }
 
             fR.onload = (e) => {
                 obj.LoadData(e.target.result);
                 getValues(obj.data); 
+                obj.placeLevel();
+                console.log(obj.sortedData);
             }
             fR.readAsText(file.files[0], "UTF-8");
         }
@@ -39,7 +72,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }
     })
 
-//событие по нажатии кнопки start--------------------------------------------
+/**событие по нажатии кнопки start--------------------------------------------*/
 startButton.addEventListener('click', () => {
     if ( typeof (file.files[0]) === "undefined" || nameObjectWindow.value==""){
         msgbox.classList.add('active');
@@ -56,44 +89,44 @@ msgboxExit.addEventListener('click', ()=>{
 })
 
 //конфигурации графика--------------------------------------------
-    var config = {
-        type: 'bar',
-        data: {
-            labels: ["default"],
-            datasets: [
-                highArr={
-                    label: ['Delenya'],
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: ['rgb(207, 0, 0, 0.5)','rgb(54, 162, 235)','rgb(154, 162, 235)'],
-                    borderColor: 'red',
-                    borderWidth: 2,
-                    order:2
-                },
-                {
-                    type: 'line',
-                    label: 'Line',
-                    data: [1,2,3],
-                    fill: false,
-                    borderColor: 'rgb(54, 162, 235)',
-                    borderWidth: 2,
-                    order:1
-                },
-            ]
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    },
-                }],
+var config = {
+    type: 'bar',
+    data: {
+        labels: labelsArrForChart,
+        datasets: [
+            {
+                label: [''],
+                data: numbersArrForChart,
+                backgroundColor: backgroundColorsArrForChart,
+                borderColor: borderColorsArrForChart,
+                borderWidth: 2,
+                order:2
             },
-        }
-    };
+            {
+                type: 'line',
+                label: 'Line',
+                data: [],
+                fill: false,
+                borderColor: 'rgb(54, 162, 235)',
+                borderWidth: 2,
+                order:1
+            },
+        ]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                },
+            }],
+        },
+    }
+};
 
-    new Chart (ctx,config);
-
+new Chart (ctx,config);
     
 })
+
