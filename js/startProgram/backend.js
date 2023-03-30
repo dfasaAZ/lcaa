388cb8b124f77f90a2af34059a2465e1a69b2b48
Nikita Dovgal <nikitadovgalkubsau@yandex.ru>
2023-03-30 09:09:44 +0300

    Finished 3 steps in calculate depart

--- js/startProgram/backend.js
+++ js/startProgram/backend.js
@@ -76,8 +76,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 /** 2 ЭТАП::: Заполнение таблиц переходов */
                 let configNumber = 1;
                 for (let i of object.counts){
-                    let totalTrans=0;
-
+                    console.log(i);
                     transitions.insertAdjacentHTML('beforeend', `
                     <div class="transitionsLine">
                         <div class="section">
@@ -91,56 +90,42 @@ document.addEventListener("DOMContentLoaded", ()=>{
                                 </div>
                                 <div class="row">
                                     <div class="confColumn"><div>${configNumber} config</div></div>
-                                    <div class="contains">
-                                        <div class="contain">
-                                            <div class="transFrom"></div>
-                                            <div class="transTo"></div>
-                                            <div class="countTrans"></div>
-                                            <div class="totalTrans"></div>
-                                        </div>
-                                    </div>
+                                    <div class="contains"></div>
                                 </div>   
                             </div>
                         </div>
                     </div>
                     `)
 
-                        let k=0;
-                        let k1=true;
-                        Object.entries(i).forEach(e=>{
-                            let string = Array.from(e[0]);
-                            if (k1) {
-                                document.querySelectorAll('.contains .contain .transFrom')[document.querySelectorAll('.contains .contain .transFrom').length-1].insertAdjacentHTML('beforeend', `<div>${string.slice(0,string.length-1).join("")}</div>`);
-                                k1=false;
-                            }
 
-                            if (k<3){
-                                document.querySelectorAll('.contains .contain .transTo')[document.querySelectorAll('.contains .contain .transTo').length-1].insertAdjacentHTML('beforeend', `<div>${string.slice(-1)}</div>`);
-                                document.querySelectorAll('.contains .contain .countTrans')[document.querySelectorAll('.contains .contain .countTrans').length-1].insertAdjacentHTML('beforeend', `<div>${e[1]}</div>`);
-                                totalTrans+=e[1];
-                                k++;
-                            }else{
-                                document.querySelectorAll('.contains .contain .totalTrans')[document.querySelectorAll('.contains .contain .totalTrans').length-1].insertAdjacentHTML('beforeend', `<div>${totalTrans}</div>`);
-                                totalTrans=0;
-
-                                document.querySelectorAll('.row .contains')[document.querySelectorAll('.row .contains').length-1].insertAdjacentHTML('beforeend',
-                                    `<div class="contain">
-                                        <div class="transFrom"></div>
-                                        <div class="transTo"></div>
-                                        <div class="countTrans"></div>
-                                        <div class="totalTrans"></div>
+                    let t = 0;
+                    let totalTrans = 0;
+                    for (t = 0; t<Object.entries(i).length; t+=3){
+                        totalTrans = Object.entries(i)[t][1]+Object.entries(i)[t+1][1]+Object.entries(i)[t+2][1];
+                        document.querySelectorAll('.transitionsLine .table .row .contains')[document.querySelectorAll('.transitionsLine .table .row').length-1].insertAdjacentHTML('beforeend', 
+                        `
+                                <div class="contain">
+                                    <div class="transFrom">${Object.entries(i)[t][0].slice(0,Object.entries(i)[t][0].length-1)}</div>
+                                    <div class="transTo">
+                                        <div>${Object.entries(i)[t][0].slice(-1)}</div>
+                                        <div>${Object.entries(i)[t+1][0].slice(-1)}</div>
+                                        <div>${Object.entries(i)[t+2][0].slice(-1)}</div>
                                     </div>
-                                `);
-                                k=0;
-                                k1=true;
-                            }
-                        })
-                     
+                                    <div class="countTrans">
+                                        <div>${Object.entries(i)[t][1]}</div>
+                                        <div>${Object.entries(i)[t+1][1]}</div>
+                                        <div>${Object.entries(i)[t+2][1]}</div>
+                                    </div>
+                                    <div class="totalTrans">${totalTrans}</div>
+                                </div>
+                        `)
+
+                        totalTrans = 0;
+                    }
 
 
                     configNumber++;
                 }
-
 
 
                 /** 3 ЭТАП::: Заполнение таблиц валидации */
@@ -153,7 +138,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
                     let middleArrP = [];
                     let highArrP = [];
                     [lowArrP,middleArrP,highArrP] = Object.entries(i)[4][1].entries(); //деструктуризация мэпа вероятностей
-                    console.log(Object.entries(i));
 
                     termsLine.insertAdjacentHTML('beforeend', `
                     <div class="instance">
